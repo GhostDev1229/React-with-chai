ee61c4d16ab9fada1a40f695f8d0bd822a4efeb6
Jinay Shah <shahjinay866@gmail.com>
2026-02-01 15:56:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function App() {
+
+  let[counter,setCounter]=useState(5);
+
+  const addValue = () => {
+  setCounter(counter+1);
+  }
+
+   const decValue = () => {
+    if(counter<=0) return;
+  setCounter(counter-1) ;
+  }
+
+  return (
+    <>
+    <h1>chai aur react</h1>
+    <h2>counter value: {counter} </h2>
+
+    <button onClick={addValue}>add value</button>
+    <br />
+    <button onClick={decValue}>Decrese value</button>
+    </>
+  )
+}
+
+export default App
